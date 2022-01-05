921760d18c7e15f7b941d1a2e959476d9df6e48e
‘Katie <ktschr11@gmail.com>
2022-01-05 13:53:26 -0800

    fixed sign out button by tweaking the conditional in the private route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,17 +1,19 @@
 import { Redirect, Route } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
+import Auth from '../../views/Auth/Auth.jsx';
 
 // A wrapper for <Route> that redirects to the login
 // screen if you're not yet authenticated.
 export default function PrivateRoute({ children, ...rest }) {
   // TODO: Use the user in context to determine whether to redirect to /login
   const { user } = useUser();
+  console.log(user);
 
   return ( 
     <Route 
       {...rest}
       render={({ location }) => 
-        user ? 
+        user.aud ==='authenticated' ? 
         children : 
         <Redirect 
           to={{
